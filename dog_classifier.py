a371f4fe5c66906589d7289866e377bbaaaae4dd
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2022-03-17 13:05:28 +0100

    Update dog_classifier.py

--- dog_classifier.py
+++ dog_classifier.py
@@ -16,16 +16,7 @@ def predict(img):
     prob = round(torch.max(probs).item() * 100, 2)
     st.success(f'This is a {clas} with a probability of {prob}%.')
 
-option = st.radio('', ['Choose a test image', 'Choose your own image'])
 
-if option == 'Choose a test image':
-    test_images = os.listdir('images/')
-    test_image = st.selectbox('Please select a test image:', test_images)
-    file_path = 'images/' + test_image
-    img = PILImage.create(file_path)
-    predict(img)
-
-else:
     uploaded_file = st.file_uploader('Please upload an image', type='jpg')
     if uploaded_file is not None:
         img = PILImage.create(uploaded_file)
