485a187e983fc96488ce837119d5cce9fcb01e29
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2022-03-17 23:10:58 +0100

    Update dog_classifier.py

--- dog_classifier.py
+++ dog_classifier.py
@@ -24,3 +24,32 @@ uploaded_file = st.file_uploader('Please upload an image', type=['png','jpeg', '
 if uploaded_file is not None:
  img = PILImage.create(uploaded_file)
  predict(img)
+
+    
+if st.sidebar.button("Click Here to Classify"):
+
+    if CT_Image is None:
+
+        st.sidebar.write("Please upload an Image to Classify")
+
+    else:
+        with st.spinner('Classifying ...'):
+            Category = ["COVID", "Non-COVID"]
+            pred = model.predict((my_image))
+
+            if pred[0][0] > 0.5:
+                Cat = "Non-COVID"
+            else:
+                Cat = "COVID"
+
+
+            time.sleep(2)
+            st.sidebar.success('Done!')
+
+            st.sidebar.header("Algorithm Predicts: ")
+            if Cat == "COVID":
+                st.sidebar.write("The lung is classified as **COVID-19 affected**")
+            elif Cat == "Non-COVID":
+                st.sidebar.write("The lung is classified as **Healthy**")
+            else:
+                st.sidebar.write("ciao")
