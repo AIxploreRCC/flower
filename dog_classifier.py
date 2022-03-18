b793b275aacade0d8d80a7efb662d345ed76d523
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2022-03-18 10:28:42 +0100

    Update dog_classifier.py

--- dog_classifier.py
+++ dog_classifier.py
@@ -2,6 +2,9 @@ from fastai.vision.all import load_learner, torch, PILImage
 import streamlit as st
 import os
 import time
+import numpy as np # for image processing 
+from PIL import Image #Image processing 
+import cv2 #computer vision 
 
 st.set_page_config('flower classification')
 st.title('flower classification')
@@ -16,13 +19,18 @@ def predict(img):
     clas, clas_idx, probs = learn.predict(img)
     prob = round(torch.max(probs).item() * 100, 2)
     st.success(f'This is a {clas} with a probability of {prob}%.')
+
+file_image = st.camera_input(label = "Take a pic of you to be sketched out")
+
     
-st.radio('', ['Choose your own image'])
+option = st.radio('', ['Take a photo', 'Choose your own image'])
 
-uploaded_file = st.file_uploader('Please upload an image', type=['png','jpeg', 'jpg'])
-
-if uploaded_file is not None:
- img = PILImage.create(uploaded_file)
- predict(img)
-
+if option == 'Choose a test image':
+    uploaded_file = st.file_uploader('Please upload an image', type=['png','jpeg', 'jpg'])
+    if uploaded_file is not None:
+        img = PILImage.create(uploaded_file)
+        predict(img)
+        
+else:
+    input_img = Image.open(file_image)
 
